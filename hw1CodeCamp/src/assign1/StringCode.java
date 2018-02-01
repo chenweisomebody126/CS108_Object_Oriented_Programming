f7f560b4f37ad6313bd7f7c448903095c98b7efe
chen4626 <chen4626@umn.edu>
2018-01-31 22:07:07 -0800

    add: basecode for hw1

---
+++ hw1CodeCamp/src/assign1/StringCode.java
@@ -0,0 +1,32 @@
+package assign1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// CS108 HW1 -- String static methods
+
+public class StringCode {
+
+	/**
+	 * Given a string, returns the length of the largest run.
+	 * A a run is a series of adajcent chars that are the same.
+	 * @param str
+	 * @return max run length
+	 */
+	public static int maxRun(String str) {
+		return 0; // TODO ADD YOUR CODE HERE
+	}
+
+	
+	/**
+	 * Given a string, for each digit in the original string,
+	 * replaces the digit with that many occurrences of the character
+	 * following. So the string "a3tx2z" yields "attttxzzz".
+	 * @param str
+	 * @return blown up string
+	 */
+	public static String blowup(String str) {
+		return null; // TODO ADD YOUR CODE HERE
+	}
+	
+}
