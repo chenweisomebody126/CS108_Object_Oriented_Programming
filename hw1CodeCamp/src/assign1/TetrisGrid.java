eef8b53f0aec95580f274226b3ce6cdbc7a56d88
chen4626 <chen4626@umn.edu>
2018-02-02 20:44:59 -0800

    add: clearRows in TerisGrid

--- hw1CodeCamp/src/assign1/TetrisGrid.java
+++ hw1CodeCamp/src/assign1/TetrisGrid.java
@@ -4,34 +4,40 @@
 package assign1;
 
 public class TetrisGrid {
-	
+	private boolean[][] grid;
 	/**
 	 * Constructs a new instance with the given grid.
 	 * Does not make a copy.
 	 * @param grid
 	 */
 	public TetrisGrid(boolean[][] grid) {
+		this.grid = grid;
 	}
 	
 	
 	/**
 	 * Does row-clearing on the grid (see handout).
 	 */
-	public void clearRows(boolean[][] grid) {
+	public void clearRows() {
 		if (grid == null || grid.length ==0 || grid[0] == null || grid[0].length ==0)
 			return;
-		for (int i=0; i< grid.length; i++) {
-			boolean fullRow = true;
-			for (int j=0; j< grid[i].length; j++) {
-				if (grid[i][j] == false)
-					fullRow = false;
+		// loop over the bottom row
+		boolean fullRow = true;
+		for (int j =0; j<grid[grid.length-1].length ; j++) {
+			if (grid[grid.length-1][j]== false) {
+				fullRow = false;
 			}
-			if (fullRow == true) {
-				for (int j=0; j<grid[i].length; j++) {
-					if (grid[i][j] == true)
-						grid[i][j] = false; 
-				}
+		}
+		if (fullRow == false)
+			return;
+		for (int i= grid.length-1; i>0; i++) {
+			for (int j =0; j< grid[i].length; j++) {
+				grid[i][j] = grid[i-1][j];
 			}
+		}
+		
+		for (int j= 0; j<grid[0].length; j++) {
+			grid[0][j] = false;
 		}
 		return;
 	}
@@ -41,6 +47,6 @@ public class TetrisGrid {
 	 * @return 2d grid array
 	 */
 	boolean[][] getGrid() {
-		return null; // TODO YOUR CODE HERE
+		return grid; // TODO YOUR CODE HERE
 	}
 }
