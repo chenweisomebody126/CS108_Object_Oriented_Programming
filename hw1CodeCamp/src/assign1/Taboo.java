2afbbe077048b9cee8858351e2440be1757c3ce9
chen4626 <chen4626@umn.edu>
2018-02-03 17:11:07 -0800

    add: reduce in Taboo

--- hw1CodeCamp/src/assign1/Taboo.java
+++ hw1CodeCamp/src/assign1/Taboo.java
@@ -45,5 +45,27 @@ public class Taboo<T> {
 	 * @param list collection to reduce
 	 */
 	public void reduce(List<T> list) {
+		if (list== null || list.isEmpty())
+			return;
+		HashMap<T, Set<T>> noFollowMap = new HashMap<T, Set<T>>(); 
+		List<T> reducedList = new ArrayList<T>();
+		reducedList.add(list.get(0));
+		for (int i=0, size = list.size(); i<size-1; i++) {
+			T elem= list.get(i);
+			if (noFollowMap.containsKey(elem)) {
+				if (! noFollowMap.get(elem).contains(list.get(i+1))) {
+					reducedList.add(list.get(i+1));
+				}
+			} else {
+				Set<T> tabooSet = noFollow(list.get(i));
+				if (! tabooSet.contains(list.get(i+1))) {
+					reducedList.add(list.get(i+1));
+				}
+				noFollowMap.put(list.get(i), tabooSet);
+			} 
+		 }
+		list = reducedList;
 	}
+	
+
 }
