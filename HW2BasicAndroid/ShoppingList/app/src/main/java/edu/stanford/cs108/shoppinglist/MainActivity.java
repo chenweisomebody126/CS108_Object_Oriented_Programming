192d12384ad7bf840633cb280cf99f1e89ba9236
chen4626 <chen4626@umn.edu>
2018-02-11 14:35:45 -0800

    add: shopping list

---
+++ HW2BasicAndroid/ShoppingList/app/src/main/java/edu/stanford/cs108/shoppinglist/MainActivity.java
@@ -0,0 +1,28 @@
+package edu.stanford.cs108.shoppinglist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void add2ShoppingList(View view) {
+        EditText item = (EditText) findViewById(R.id.editText);
+        TextView shoppingList = (TextView) findViewById((R.id.textView));
+        shoppingList.append(item.getText().toString()+"\n");
+        item.setText("");
+    }
+    public void clearShoppingList(View view){
+        TextView shoppingList = (TextView) findViewById((R.id.textView));
+        shoppingList.setText("");
+    }
+}
